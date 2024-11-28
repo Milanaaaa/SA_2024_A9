5fd48db6216211365be34d793f99bba80c6c7955
anijackich <anijackich@gmail.com>
2024-11-28 20:49:50 +0300

    publish service refactoring

--- event-driven/services/publish_service.py
+++ event-driven/services/publish_service.py
@@ -10,22 +10,25 @@ load_dotenv()
 EMAIL_ADDRESS = os.getenv("EMAIL_ADDRESS")
 EMAIL_PASSWORD = os.getenv("EMAIL_PASSWORD")
 
-recepecients = [
+recipients = [
     "mikhail_kalinin_a@mail.ru",
     "fragile_cat@mail.ru",
     "emi.gaynulllin@gmail.com",
 ]
 
+
 def send_email(message):
     with SMTP_SSL("smtp.mail.ru", 465) as smtp:
         smtp.login(user=EMAIL_ADDRESS, password=EMAIL_PASSWORD)
+
         msg = MIMEText(
-            f"From user: {message['user_alias']}\nMessage: {
-                        message['message']}"
+            f"From user: {message['user_alias']}\n"
+            f"Message: {message['message']}"
         )
+
         msg["Subject"] = "SoftArch assignment"
-        
-        for rec in recepecients:
+
+        for rec in recipients:
             smtp.sendmail(EMAIL_ADDRESS, rec, msg.as_string())
 
 
@@ -34,13 +37,23 @@ def callback(ch, method, properties, body):
     send_email(message)
 
 
-connection = pika.BlockingConnection(
-    pika.ConnectionParameters(host="localhost", port=5672)
-)
-channel = connection.channel()
-channel.queue_declare(queue="screamed_messages")
-channel.basic_consume(
-    queue="screamed_messages", on_message_callback=callback, auto_ack=True
-)
-print("Publish Service: Waiting for messages...")
-channel.start_consuming()
+def main():
+    connection = pika.BlockingConnection(
+        pika.ConnectionParameters(host="localhost", port=5672)
+    )
+
+    channel = connection.channel()
+
+    channel.queue_declare(queue="screamed_messages")
+
+    channel.basic_consume(
+        queue="screamed_messages", on_message_callback=callback, auto_ack=True
+    )
+
+    print("Publish Service: Waiting for messages...")
+
+    channel.start_consuming()
+
+
+if __name__ == '__main__':
+    main()
