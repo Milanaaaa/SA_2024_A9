b0d1ef3a519b4ad3895e6aa239da30d527fdd9cb
anijackich <anijackich@gmail.com>
2024-11-28 20:45:33 +0300

    filter service refactoring

--- event-driven/services/filter_service.py
+++ event-driven/services/filter_service.py
@@ -1,32 +1,53 @@
 import pika
 import json
 
-STOP_WORDS = {"bird-watching", "ailurophobia", "mango"}
+STOP_WORDS = [
+    "bird-watching",
+    "ailurophobia",
+    "mango"
+]
 
 
 def callback(ch, method, properties, body):
     message = json.loads(body)
+
     if any(word in message["message"] for word in STOP_WORDS):
         print("Message filtered out:", message["message"])
         return
+
+    with pika.BlockingConnection(
+            pika.ConnectionParameters(host="localhost", port=5672)
+    ) as connection:
+        channel = connection.channel()
+
+        channel.queue_declare(queue="filtered_messages")
+
+        channel.basic_publish(
+            exchange="",
+            routing_key="filtered_messages",
+            body=json.dumps(message)
+        )
+
+
+def main():
     connection = pika.BlockingConnection(
         pika.ConnectionParameters(host="localhost", port=5672)
     )
+
     channel = connection.channel()
-    channel.queue_declare(queue="filtered_messages")
-    channel.basic_publish(
-        exchange="", routing_key="filtered_messages", body=json.dumps(message)
+
+    channel.queue_declare(queue="incoming_messages")
+
+    channel.basic_consume(
+        queue="incoming_messages",
+        on_message_callback=callback,
+        auto_ack=True
     )
-    connection.close()
+
+    print("Filter Service: Waiting for messages...")
+
+    channel.start_consuming()
 
 
-connection = pika.BlockingConnection(
-    pika.ConnectionParameters(host="localhost", port=5672)
-)
-channel = connection.channel()
-channel.queue_declare(queue="incoming_messages")
-channel.basic_consume(
-    queue="incoming_messages", on_message_callback=callback, auto_ack=True
-)
-print("Filter Service: Waiting for messages...")
-channel.start_consuming()
+if __name__ == "__main__":
+    main()
