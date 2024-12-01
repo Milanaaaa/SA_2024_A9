24770f6cbfa0f6d236b4fd7c8c44a1492a0be94f
Roman Pogrebnyak <grabberisonfire@gmail.com>
2024-12-02 00:35:11 +0300

    pipes-and-filters-done

--- pipes-and-filters/services/filter_service.py
+++ pipes-and-filters/services/filter_service.py
@@ -1,31 +1,32 @@
 from flask import Flask, request, jsonify
 import requests
-import json
 
-# Define downstream service endpoint (screaming_service)
 SCREAMING_SERVICE_URL = "http://screaming_service:5002/process"
 
 STOP_WORDS = ["bird-watching", "ailurophobia", "mango"]
 
 app = Flask(__name__)
 
+
 @app.route('/process', methods=['POST'])
 def process_message():
     data = request.get_json()
 
-    # Check for invalid payloads
     if not data or 'message' not in data:
         return jsonify({'error': 'Invalid payload'}), 400
 
-    # Apply filtering logic
     if any(word in data["message"] for word in STOP_WORDS):
-        print(f"Filter Service: Message filtered out: {data['message']}")
         return jsonify({'status': 'Filtered out'}), 200
 
-    # Forward to screaming_service
     response = requests.post(SCREAMING_SERVICE_URL, json=data)
-    print("Filter Service: Forwarded message to screaming service")
-    return jsonify({'status': 'Message forwarded', 'screaming_response': response.json()})
+
+    if response.status_code >= 200 and response.status_code < 300:
+        return jsonify({'status': 'Message forwarded',
+                        'screaming_response': response.json()})
+    else:
+        return jsonify({'status': 'error',
+                        'error_details': response.text}), response.status_code
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5001)
--- pipes-and-filters/services/screaming_service.py
+++ pipes-and-filters/services/screaming_service.py
@@ -1,27 +1,29 @@
 from flask import Flask, request, jsonify
 import requests
-import json
 
-# Define downstream service endpoint (publish_service)
 PUBLISH_SERVICE_URL = "http://publish_service:5003/process"
 
 app = Flask(__name__)
+
 
 @app.route('/process', methods=['POST'])
 def process_message():
     data = request.get_json()
 
-    # Check for invalid payloads
     if not data or 'message' not in data:
         return jsonify({'error': 'Invalid payload'}), 400
 
-    # Transform the message
     data['message'] = data['message'].upper()
-    print(f"SCREAMING Service: Transformed Message: {data}")
 
-    # Forward to publish_service
     response = requests.post(PUBLISH_SERVICE_URL, json=data)
-    return jsonify({'status': 'Message forwarded', 'publish_response': response.json()})
+
+    if response.status_code >= 200 and response.status_code < 300:
+        return jsonify({'status': 'Message forwarded',
+                        'publish_response': response.json()})
+    else:
+        return jsonify({'status': 'error',
+                        'error_details': response.text}), response.status_code
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5002)
